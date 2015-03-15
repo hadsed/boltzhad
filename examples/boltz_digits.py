fixed error with bias gradient calculation. added random filter sampling to get an idea of learned features for binary alphadigits.

examples/boltz_digits.py
@@ -44,7 +44,7 @@ nvisible = 320
 # number of hidden units
 # nhidden = 50
 # for plotting
-nhidrow = 5  # essentially unbounded
+nhidrow = 8  # essentially unbounded
 nhidcol = 16  # should be less than 16
 nhidden = nhidrow*nhidcol
 # number of MCMC steps in CD
@@ -52,12 +52,12 @@ cdk = 3
 # size of the minibatch for each gradient update
 batchsize = 1
 # learning rate
-eta = 0.007
+eta = 0.001
 # training epochs
 # (if we're low on data, we can set this higher)
 epochs = 1000
 # weight decay term
-wdecay = 0.002
+wdecay = 0.0005
 # Random number generator
 seed = None
 rng = np.random.RandomState(seed)
@@ -70,7 +70,8 @@ classes = [10,11,12]
 # max training vectors is 39
 nperclass = 29
 # up-down iterations for sampling trained network
-kupdown = 100
+kupdown_inp = 10
+kupdown_filter = 1000
 
 # training data
 datamat = sio.loadmat('data/binaryalphadigs.mat')
@@ -94,13 +95,14 @@ boltz.train_restricted(datasp, W, vbias, hbias, eta, wdecay,
                        epochs, cdk, batchsize, rng)
 print("Training complete.")
 # plot stuff
-fig, ax = plt.subplots(1+2*len(classes),1, figsize=(6,10))
+fig, ax = plt.subplots(1+2*len(classes),2, figsize=(9,10))
 border = 0
 # create a matrix to hold all the stuff we want to plot
 trainmat = -1.0*np.ones((20*len(classes), nperclass*16))
 inpmat = -1.0*np.ones((20+nhidrow+1, samples*(16+border)))
 outmat = -1.0*np.ones((20+nhidrow+1, samples*(16+border)))
 classmats = {}
+filtermats = {}
 # first row is the training images
 for itvec, tvec in enumerate(datasp.T):
     # how many training per class
@@ -115,10 +117,41 @@ for itvec, tvec in enumerate(datasp.T):
 for icls, cls in enumerate(classes):
     classmats[cls] = { 'inp': inpmat.copy(), 
                        'out': outmat.copy() }
+    filtermats[cls] = { 'inp': inpmat.copy(), 
+                        'out': outmat.copy() }
+    # initialize a random state for filters
+    filterstate1 = np.asarray(np.random.binomial(1, 0.5, nvisible+nhidden),
+                              dtype=np.float)
+    filterstate2 = np.asarray(np.random.binomial(1, 0.5, nvisible+nhidden),
+                              dtype=np.float)
     # gather samples and add them to plotting matrix
     for isample in xrange(samples):
+        colidx = isample*16
+        colidxh = colidx
         # choose input vector (not from training data)
         inpidx = nperclass+isample-1
+        # propagate random state to get some filter
+        filterstate1 = boltz.sample_restricted(
+            filterstate1.reshape(filterstate1.size,1), 
+            W, vbias, hbias, kupdown_filter
+        )
+        # output visibles
+        filtermats[cls]['inp'][:20,colidx:colidx+16] = \
+                    filterstate1[:nvisible].reshape(20,16).astype(int)
+        # output hiddens
+        filtermats[cls]['inp'][21:21+nhidrow,colidxh:colidxh+nhidcol] = \
+                    filterstate1[nvisible:].reshape(nhidrow,nhidcol).astype(int)
+        # propagate random state to get some filter (again)
+        filterstate2 = boltz.sample_restricted(
+            filterstate2.reshape(filterstate2.size,1), 
+            W, vbias, hbias, kupdown_filter
+        )
+        # output visibles
+        filtermats[cls]['out'][:20,colidx:colidx+16] = \
+                    filterstate2[:nvisible].reshape(20,16).astype(int)
+        # output hiddens
+        filtermats[cls]['out'][21:21+nhidrow,colidxh:colidxh+nhidcol] = \
+                    filterstate2[nvisible:].reshape(nhidrow,nhidcol).astype(int)
         # initialize a random state
         state = np.asarray(np.random.binomial(1, 0.5, nvisible+nhidden),
                            dtype=np.float)
@@ -128,8 +161,6 @@ for icls, cls in enumerate(classes):
         state[nvisible:] = (logit(np.dot(W.T, state[:nvisible])) > 
                             np.random.rand(nhidden))
         # input visible units
-        colidx = isample*16
-        colidxh = colidx
         classmats[cls]['inp'][:20,colidx:colidx+16] = \
                                 state[:nvisible].reshape(20,16).astype(int)
         # input hidden units
@@ -138,7 +169,7 @@ for icls, cls in enumerate(classes):
         # do some up-down samples
         print("Sampling for input #"+str(isample))
         state = boltz.sample_restricted(state.reshape(state.size,1), 
-                                        W, vbias, hbias, kupdown)
+                                        W, vbias, hbias, kupdown_inp)
         print("Sampling #"+str(isample)+" done.")
         # output visibles
         classmats[cls]['out'][:20,colidx:colidx+16] = \
@@ -157,19 +188,38 @@ for k in classmats.iterkeys():
     classmats[k]['out'][classmats[k]['out'] == 0] = -2
     classmats[k]['out'][classmats[k]['out'] == -1] = 0
     classmats[k]['out'][classmats[k]['out'] == -2] = -1
+    filtermats[k]['inp'][filtermats[k]['inp'] == 0] = -2
+    filtermats[k]['inp'][filtermats[k]['inp'] == -1] = 0
+    filtermats[k]['inp'][filtermats[k]['inp'] == -2] = -1
+    filtermats[k]['out'][filtermats[k]['out'] == 0] = -2
+    filtermats[k]['out'][filtermats[k]['out'] == -1] = 0
+    filtermats[k]['out'][filtermats[k]['out'] == -2] = -1
 # plot them
-ax[0].set_title("Training data")
-ax[0].matshow(trainmat, cmap=matplotlib.cm.binary)
+ax[0,0].set_title("Training data")
+ax[0,1].set_title("Training data")
+ax[0,0].matshow(trainmat[:,:8*nperclass], cmap=matplotlib.cm.binary)
+ax[0,1].matshow(trainmat[:,8*nperclass:], cmap=matplotlib.cm.binary)
 # input rows
 iax = 1
 for val in classmats.itervalues():
-    ax[iax].set_title("Inputs (visible then hidden units)")
-    ax[iax].matshow(val['inp'], cmap=matplotlib.cm.binary)
-    ax[iax+1].set_title("Result (after "+str(kupdown)+" up-down samples)")
-    ax[iax+1].matshow(val['out'], cmap=matplotlib.cm.binary)
+    ax[iax,0].set_title("Inputs (visible then hidden)")
+    ax[iax,0].matshow(val['inp'], cmap=matplotlib.cm.binary)
+    ax[iax+1,0].set_title("Result ("+str(kupdown_inp)+" Gibbs steps)")
+    ax[iax+1,0].matshow(val['out'], cmap=matplotlib.cm.binary)
+    iax += 2
+# random filter rows
+iax = 1
+for val in filtermats.itervalues():
+    ax[iax,1].set_title("Filters (left to right "+
+                        str(kupdown_filter)+" Gibbs steps)")
+    ax[iax,1].matshow(val['inp'], cmap=matplotlib.cm.binary)
+    ax[iax+1,1].set_title("Filters (left to right "+
+                        str(kupdown_filter)+" Gibbs steps)")
+    ax[iax+1,1].matshow(val['out'], cmap=matplotlib.cm.binary)
     iax += 2
 # remove axes
-for kax in ax:
-    kax.get_xaxis().set_visible(False)
-    kax.get_yaxis().set_visible(False)
+for r in ax:
+    for kax in r:
+        kax.get_xaxis().set_visible(False)
+        kax.get_yaxis().set_visible(False)
 plt.show()